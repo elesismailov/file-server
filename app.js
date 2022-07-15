a0c2f910fb815c0dd1342d653c5c81b9fc5974c4
eles linux <eleswastaken@gmail.com>
2022-07-15 10:49:27 +0600

    Initialize project

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+const express = require('express');
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+
+  res.send('hello world!')
+})
+
+
+const port = 5000;
+
+
+app.listen(port, () => {
+  console.log('ready')
+})
