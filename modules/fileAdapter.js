6fe9e909e56c7881c895a874cee29fc4d9169ec7
eles linux <eleswastaken@gmail.com>
2022-07-15 13:22:13 +0600

    implement adapter.getFile method

--- modules/fileAdapter.js
+++ modules/fileAdapter.js
@@ -1,19 +1,34 @@
+const path = require('path');
+const fs = require('fs');
 
-function Adapter(filepath) {
+function Adapter(dirpath) {
+
+  this.path = path.parse(dirpath).dir;
 
 }
 
+
 Adapter.prototype.getFile = function(filename) {
-  // returns a file stream   
+  // returns a file stream
+  console.log('get file')
+
+  const filepath = path.join(this.path, filename);
+
+  const readStream = fs.createReadStream(filepath);
+
+  return readStream
+
 }
 
 Adapter.prototype.createFile = function(filename, stream) {
   // creates file
+  console.log('create file')
 }
 
 Adapter.prototype.ls = function() {
   // returns a str list of all files
+  console.log('list files')
 }
 
-export default Adapter;
 
+module.exports = Adapter;
--- modules/fileStorage.js
+++ modules/fileStorage.js
@@ -1,9 +1,13 @@
 
+const Adapter = require('./fileAdapter');
 const fileStorage = {};
 
 fileStorage.createAdapter = function(path) {
 
-  console.log('created adapter')
+  console.log('created adapter ' + path)
+
+  return new Adapter(path);
+
 }
 
 
