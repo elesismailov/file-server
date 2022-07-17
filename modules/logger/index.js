d2e6e4d79626f5e3aa6b92bf9a21df263ab9acd1
eles linux <eleswastaken@gmail.com>
2022-07-17 17:15:26 +0600

    Todo

--- modules/logger/index.js
+++ modules/logger/index.js
@@ -4,6 +4,15 @@ function logger(req, res, next) {
 
   next();
 
+  /* TODO
+
+    - info: if number of files exceeds <settings.fileExeed>
+    - log: started sending file if <settings.upload>
+    - log: started recieving file if <settings.download>
+    - warn: size of files in <certain> dir exeeded if <settings.size> != null
+
+  */
+
 }
 
 
