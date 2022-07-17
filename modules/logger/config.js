b629f9be39ec1f490fc504b5d4d5201146ef4cfd
eles linux <eleswastaken@gmail.com>
2022-07-17 17:24:08 +0600

    Add logger config

---
+++ modules/logger/config.js
@@ -0,0 +1,18 @@
+
+const loggerConfig = {
+
+
+  // enable to log sending files to client
+  send: true,
+
+  // enable to log reveiving files from the client
+  receive: true,
+
+  // WARN if the number of files in a directory exeeds this number
+  fileNumberExeed: 3,
+
+  // WARN if the size of the files in a dir
+  // exeeds this number in KB
+  dirSize: 10,
+
+}
