51266cba2056dc338a986049d6c4cf306f35c21e
eles linux <eleswastaken@gmail.com>
2022-07-18 11:15:01 +0600

    Create some tests

--- tests/metaStorage.test.js
+++ tests/metaStorage.test.js
@@ -1,11 +1,15 @@
 
 require('dotenv').config()
 
-const initializeMongo = require('../mongoConfig');
 
 const metaStorage = require('../modules/metaStorage.js');
 
+let path;
+
 beforeAll(() => {
+
+  path = require('path');
+  const initializeMongo = require('../mongoConfig');
 
   return initializeMongo()
 
@@ -19,6 +23,20 @@ describe('Test getMetaData', () => {
 
     expect(meta).toBeNull();
     
+  });
+
+
+  test('get meta data', async () => {
+
+    metaStorage.createMetaData(path.join(__dirname, '/data/test1.txt'))
+
+    const meta = await metaStorage.getMetaData(path.join(__dirname, '/data/test1.txt'));
+
+    expect(meta).not.toBeNull();
+    expect(meta).toHaveProperty('dir');
+
+
+
   });
 
 });
